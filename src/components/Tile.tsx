c9fdc507a76b48d1d90f771e536c1c44d43cd5fb
Najim Rizky <najim.rizky@student.umn.ac.id>
2022-08-03 19:22:00 +0700

    fix bug layout tiles

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -15,7 +15,7 @@ const Tile = (props: Props) => {
             <Box onClick={() => props.flipTile(props.id)} className={`${props.flipped.includes(props.id) || props.trueFlipped.includes(props.id) ? "tile flip" : "tile"} ${props.trueFlipped.includes(props.id) ? "true" : ""}`} sx={{ cursor: "pointer", height: "100%", borderRadius: "5px" }}>
                 <Box className="tile-inner" >
                     <Box className="tile-front" sx={{ borderRadius: "5px" }}></Box>
-                    <Box className="tile-back" sx={{ background: props.type === "color" ? props.tileData : "transparent", height: "100%", borderRadius: "5px" }}>
+                    <Box className="tile-back" sx={{ background: props.type === "color" ? props.tileData : "transparent", height: "100%", borderRadius: "5px", overflow: "hidden" }}>
                         {props.type !== "color" && (
                             <img width={"100%"} alt="" src={props.tileData}></img>
                         )}
